142638fcfc4ab37d477df1bf97cbf080672c19c3
Arztiser <145240129+Arztiser@users.noreply.github.com>
2024-06-17 17:25:15 -0500

    Update player.js

--- player.js
+++ player.js
@@ -8,7 +8,7 @@ const progressBar = document.querySelector('#progress-bar'); // element where pr
 let pPause = document.querySelector('#play-pause'); // element where play and pause image appears
 
 songIndex = 0;
-songs = ['./assets/music/beyonce.mp3', './assets/music/dontstartnow.mp3']; // object storing paths for audio objects
+songs = ['./assets/music/y2mate.com - Big Rich Save A Horse Ride A Cowboy Official Music Video_144p.mp3', './assets/music/y2mate.com - Blake Shelton No Body Official Music Video_144p.mp3']; // object storing paths for audio objects
 thumbnails = ['./assets/images/lemonade.png', './assets/images/dontstartnow.png']; // object storing paths for album covers and backgrounds
 songArtists = ['Beyonce', 'Dua Lipa']; // object storing track artists
 songTitles = ["Don't Hurt Yourself", "Don't Start Now"]; // object storing track titles
